4234e90de2ed5c82891be3e7e55b5af2666205ea
ShawnSwinsick <vifs888@gmail.com>
2013-07-06 23:08:38 -0600

    When someone says the name, it will give a random reply

---
+++ VifsSabot/VifsSabot/CrazyChat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    class CrazyChat
+    {
+        private List<string> Replies;
+        private static Random rnd;
+
+        public CrazyChat()
+        {
+            Replies = new List<string>();
+            rnd = new Random();
+            readInReplies();
+        }
+
+
+        private void readInReplies(){
+            var reader = new StreamReader("../../Reply.txt");
+            while(reader.Peek() >= 0){
+                Replies.Add(reader.ReadLine());
+            }
+        }
+
+        public string giveReply()
+        {
+            int r = rnd.Next(Replies.Count);
+            return Replies[r];
+
+        }
+    }
+
+    
+}
